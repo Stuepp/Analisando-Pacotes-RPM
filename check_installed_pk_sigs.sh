c4774ca77dc8ef7785372c8a6b5d08369455fa0b
Stuepp <arthurh.c@hotmail.com>
2025-09-09 15:34:45 -0300

    Código corrigido e otimizado, de acordo com os comentários feito sobre expr e sobre o grep - check_installed_pk_sigs.sh

--- check_installed_pk_sigs.sh
+++ check_installed_pk_sigs.sh
@@ -1,33 +1,25 @@
 #!/bin/sh
-# a linha acima declara o bin que executará o script
+# clear
 
-clear
-# a linha acima executa um comando shell que limpa a tela
+echo "Contando pacotes assinados e não assinados..."
 
-rpm -qa > rpm_packs_list.txt
-#packs=$(rpm -qa)
-# lista todos os pacotes instalados no sistema
-
-packs="./rpm_packs_list.txt"
-
-num_packs=0
-non_sig=0
-yes_sig=0
-
-while IFS= read -r line; do
-   ## take some action on $line
-  #rpm -qi "$line"
-  #rpm -qi "$line" | grep Signature | grep none
-  if rpm -qi "$line" | grep Signature | grep none; then
-    yes_sig=$((yes_sig+1))
-  else
-    #non_sig=$(expr $non_sig + 1)
-    non_sig=$((non_sig+1))
-  fi
-  #num_packs=$(expr $num_packs + 1)
-  num_packs=$((num_packs+1))
-done < $packs
-
-echo "Total de pacotes: $num_packs"
-echo "Total de pacotes não assinados: $non_sig"
-echo "Total de pacotes assinados: $yes_sig"
+# Usando a tag %{SIGPGP:pgpsig} que é compatível com versões mais antigas do RPM
+rpm -qa --qf '%{SIGPGP:pgpsig}\n' | awk '
+  BEGIN {
+    signed = 0
+    unsigned = 0
+  }
+  {
+    if ($0 == "(none)") {
+      unsigned++
+    } else {
+      signed++
+    }
+  }
+  END {
+    print "---"
+    print "Total de pacotes: " (signed + unsigned)
+    print "Total de pacotes assinados: " signed
+    print "Total de pacotes não assinados: " unsigned
+  }
+'
