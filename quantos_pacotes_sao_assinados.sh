d95c6f33fb24fe383f27bfd3b2e4d9c367dc97f2
Stuepp <arthurh.c@hotmail.com>
2025-09-27 00:52:15 -0300

    Script para responder Quantos pacotes são assinados?

---
+++ quantos_pacotes_sao_assinados.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# ---
+# Script para verificar a assinatura e integridade de todos os pacotes .rpm
+# em um diretório especificado.
+# Utiliza o comando 'rpm -K' (equivalente a 'rpm --checksig').
+# ---
+
+# =========== PARA OS PACOTES DE TESTE PRIMEIRO =============
+
+# 1. Defina o caminho para o diretório que contém os pacotes RPM.
+#	Altere o valor abaixo para o seu diretório
+#	Ex: RPM_DIRECTOY="home/user/Downloads/pactes_rpm"
+
+RPM_DIR="/home/stuepp/Documents/RPM-TEST-PACKS"
+
+# Contadores das metricas de assinatura dos pacotes
+pacotes_assinados=0
+pacotes_n_assinados=0
+assinatura_verificavel=0
+assinatura_n_verificavel=0
+
+
+# 2. Verifica se o diretório especificado realmente exite.
+
+if [ ! -d "$RPM_DIR" ]; then
+	echo "Erro: O diretório '$RPM_DIR' não foi encontrado."
+	echo "Por favor, edite o script e configure a variável RPM_DIR corretamente."
+	exit 1
+fi
+
+echo "Iniciando a contagem de assinaturas dos pacotes em: $RPM_DIR"
+echo "==============================================="
+
+# 3. 'shopt -s nullglob' faz com que o loop não execute se nenhum arquivo .rpm for econtrado,
+#	evitando um erro.
+
+shopt -s nullglob
+files_found=0
+
+# 4. Inicia o loop 'for' para cada arquivo que termina com .rpm no diretório.
+for pacote in "$RPM_DIR"/*.rpm; do
+	# Imprime o nome do pacote que está sendo verificado para melhor feedback.
+	echo "--> Verificando: $(basename "$pacote")"
+
+	# Executa o comando 'rpm -K' no pacote.
+	# A opção -v (verbose) pode ser adicionada para mais detalhes: rpm -Kv "$pacote"
+	saida=$(rpm -qi "$pacote" | grep "Signature")
+    if [[ "$saida" == *"(none)"* ]]; then
+        ((pacotes_n_assinados++))
+    else
+        ((pacotes_assinados++))
+    fi
+    #rpm -qi "$pacote" | grep "Signature"
+	#echo "----------------------------------------------"
+	((files_found++))
+done
+
+# Restaure o comportamento padrão do glob
+shopt -u nullglob
+
+# 5. Informa ao usuário se nenhum pacote foi encontrado.
+if [ "$files_found" -eq 0 ]; then
+	echo "Nenhum arquivo .rpm encontrado no diretório."
+else
+	echo "Verificação concluída. Total de $files_found pacotes analisados."
+    echo "Total de pacotes assinados: $pacotes_assinados."
+    echo "Total de pacotes não assinados $pacotes_n_assinados"
+fi
+
+exit 0
